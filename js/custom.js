e4cda41892c972305b3bf8d26dc885c052d94100
asd2599 <asd2590@naver.com>
2026-01-15 19:49:31 +0900

    feature section complit

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,3 @@
-/** Header Change Effect **/
-const header = document.querySelector('#header');
-
-const stickyHeader = () => {
-  const scry = window.scrollY;
-
-  if (scry > 0) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
-  }
-};
-
-window.addEventListener('scroll', stickyHeader);
-
 // Scroll Reveal Effect
 const sr = ScrollReveal({
   reset: true,
@@ -22,4 +7,17 @@ sr.reveal('.landing-text-box', {
   duration: 1000,
   origin: 'right',
   distance: '80px',
+});
+
+sr.reveal('.meet-text-box', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+});
+
+sr.reveal('.meet-wrapper img, .feature', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+  interval: 200,
 });
